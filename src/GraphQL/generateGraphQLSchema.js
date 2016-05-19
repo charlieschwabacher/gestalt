a1c167e42362bf76642ba73ee8200dce7e5774da
charlieschwabacher <charlieschwabacher@gmail.com>
2016-05-19 01:25:28 -0700

    cleanup

--- src/GraphQL/generateGraphQLSchema.js
+++ src/GraphQL/generateGraphQLSchema.js
@@ -67,72 +67,76 @@ function insertConnectionTypes(ast: Document): void {
       if (hasDirective('edge')(field)) {
         const rootType = baseType(field.type);
         const typeName = rootType.name.value;
-
         rootType.name = {kind: 'Name', value: `${typeName}Connection`};
-
         if (!definedConnections.has(typeName)) {
           definedConnections.add(typeName);
-          newDefinitions.push(
-            {
-              kind: 'ObjectTypeDefinition',
-              name: {kind: 'Name', value: `${typeName}Connection`},
-              fields: [
-                {
-                  kind: 'FieldDefinition',
-                  name: {kind: 'Name', value: 'edges'},
-                  arguments: [],
-                  type: {
-                    kind: 'ListType',
-                    type: {
-                      kind: 'NamedType',
-                      name: {kind: 'Name', value: `${typeName}Edge`}
-                    }
-                  },
-                },
-                {
-                  kind: 'FieldDefinition',
-                  name: {kind: 'Name', value: 'pageInfo'},
-                  arguments: [],
-                  type: {
-                    kind: 'NamedType',
-                    name: {kind: 'Name', value: 'PageInfo'}
-                  },
-                },
-              ],
-              interfaces: [],
-            },
-            {
-              kind: 'ObjectTypeDefinition',
-              name: {kind: 'Name', value: `${typeName}Edge`},
-              fields: [
-                {
-                  kind: 'FieldDefinition',
-                  name: {kind: 'Name', value: 'node'},
-                  arguments: [],
-                  type: {
-                    kind: 'NamedType',
-                    name: {kind: 'Name', value: typeName}
-                  },
-                },
-                {
-                  kind: 'FieldDefinition',
-                  name: {kind: 'Name', value: 'cursor'},
-                  arguments: [],
-                  type: {
-                    kind: 'NamedType',
-                    name: {kind: 'Name', value: 'String'}
-                  },
-                },
-              ],
-              interfaces: [],
-            }
-          );
+          newDefinitions.push(...makeConnectionDefintionASTNodes(typeName));
         }
       }
     });
   });
 
   ast.definitions.push(...newDefinitions);
+}
+
+function makeConnectionDefintionASTNodes(
+  typeName: string
+): [ObjectTypeDefinition, ObjectTypeDefinition] {
+  return [
+    {
+      kind: 'ObjectTypeDefinition',
+      name: {kind: 'Name', value: `${typeName}Connection`},
+      fields: [
+        {
+          kind: 'FieldDefinition',
+          name: {kind: 'Name', value: 'edges'},
+          arguments: [],
+          type: {
+            kind: 'ListType',
+            type: {
+              kind: 'NamedType',
+              name: {kind: 'Name', value: `${typeName}Edge`}
+            }
+          },
+        },
+        {
+          kind: 'FieldDefinition',
+          name: {kind: 'Name', value: 'pageInfo'},
+          arguments: [],
+          type: {
+            kind: 'NamedType',
+            name: {kind: 'Name', value: 'PageInfo'}
+          },
+        },
+      ],
+      interfaces: [],
+    },
+    {
+      kind: 'ObjectTypeDefinition',
+      name: {kind: 'Name', value: `${typeName}Edge`},
+      fields: [
+        {
+          kind: 'FieldDefinition',
+          name: {kind: 'Name', value: 'node'},
+          arguments: [],
+          type: {
+            kind: 'NamedType',
+            name: {kind: 'Name', value: typeName}
+          },
+        },
+        {
+          kind: 'FieldDefinition',
+          name: {kind: 'Name', value: 'cursor'},
+          arguments: [],
+          type: {
+            kind: 'NamedType',
+            name: {kind: 'Name', value: 'String'}
+          },
+        },
+      ],
+      interfaces: [],
+    }
+  ];
 }
 
 // attach serialization and parsing functions to scalar types defined by
