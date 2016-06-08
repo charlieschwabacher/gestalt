0221037982068b37a3038b50516fb5e4f66ad831
charlieschwabacher <charlieschwabacher@gmail.com>
2016-06-07 19:53:57 -0700

    mark tests as pending

--- packages/gestalt-postgres/test/generateRelationshipResolverTest.js
+++ packages/gestalt-postgres/test/generateRelationshipResolverTest.js
@@ -3,17 +3,17 @@ import assert from 'assert';
 describe('generateRelationshipResolver', () => {
 
   describe('loading singular relationships', () => {
-    it('loads correct results with a single query', () => {
+    xit('loads correct results with a single query', () => {
 
     });
   });
 
   describe('loading plural relationships', () => {
-    it('loads correct results', () => {
+    xit('loads correct results', () => {
 
     });
 
-    it('loads totalCount and pageInfo', () => {
+    xit('loads totalCount and pageInfo', () => {
 
     });
   });
