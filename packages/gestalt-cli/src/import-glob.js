be69c4850655fe665bc142afe78d1e39403946f4
Tyler Martinez <slightlytyler@gmail.com>
2016-07-06 17:37:21 -0500

    Remove console logs from importGlob

--- packages/gestalt-cli/src/import-glob.js
+++ packages/gestalt-cli/src/import-glob.js
@@ -3,13 +3,7 @@ import glob from 'glob';
 export default function(importPattern) {
   return glob.sync(importPattern)
     .filter(fileName => fileName.match(/\.js$/))
-    .map(fileName => {
-      console.log(fileName);
-      return require(fileName);
-    })
+    .map(fileName => require(fileName))
     .filter(module => module.__esModule)
-    .reduce((acc, module) => {
-      console.log(module);
-      return [...acc, ...Object.values(module)];
-    }, []);
+    .reduce((acc, module) => [...acc, ...Object.values(module)], []);
 }
